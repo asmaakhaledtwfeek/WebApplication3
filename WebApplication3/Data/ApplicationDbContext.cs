21ac3346289c57f84b2028fe8298806796675418
asmaakhaled <62998007+asmaakhaledtwfeek@users.noreply.github.com>
2023-02-12 20:05:34 +0200

    Initial commit

---
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models.Domins;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+       public DbSet<Test> Tests { get; set; } 
+    }
+}
---
+++ WebApplication3/Models/Domins/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models.Domins
+{
+    public class Test
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
